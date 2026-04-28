5946f5b8a01e4fa46a4e21c7e565a150940c4d69
Brayan Reyes <bryanreyes47k@gmail.com>
2026-04-28 15:44:36 -0500

    se crea controllador createDocuments de usuario-documento para el api yoplay

--- controllers/documento_controllers.go
+++ controllers/documento_controllers.go
@@ -55,3 +55,22 @@ func GetDocumentsByID(w http.ResponseWriter, r *http.Request) {
 	respondJSON(w, 200, d)
 }
 
+// POST create nuevos datos
+
+func CreateDocuments(w http.ResponseWriter, r *http.Request) {
+	var c models.documento
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	err := config.DB.QueryRow(
+		"INSERT INTO documento (tipo_documento ) VALUES ($1) RETURNING id",
+		c.Tipo_documento,
+	).Scan(&c.ID)
+
+	if err != nil {
+		respondJSON(w, 500,map[string]string{"Error":"Error"})
+		return
+	}
+	respondJSON(w, 201, map[string]string{"Message": "Dato Creado"})
+}
+
