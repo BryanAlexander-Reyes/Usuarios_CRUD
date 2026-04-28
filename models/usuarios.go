d93eecc02766ed6a00321af621373255733a1177
Brayan Reyes <bryanreyes47k@gmail.com>
2026-04-28 13:23:24 -0500

    se crea modelo de usuario para el api yoplay

---
+++ models/usuarios.go
@@ -0,0 +1,20 @@
+package models
+
+import ( 
+	"Time"
+)
+
+type User struct{
+	ID int `json:"id_usuario"` // campo id 
+	Nombre string `json:"nombre"`
+	Apellido string `json:"apellido"`
+	Numero_documento int `json:"numero_documento"`
+	Email string `json:"email"`
+	Telefono int `json:"telefono"`
+	Fecha_nacimiento time.Time `json:"fecha_nacimiento"`
+	ID_documento int `json:"id_documento"`
+	Fecha_registro string `json:"fecha_registro"`
+	Activo bool `json:"activo"`
+	Fecha_creacion time.Time `json:"fecha_creacion"`
+	Fecha_modificacion time.Time `json:"fecha_modificacion"`
+}
