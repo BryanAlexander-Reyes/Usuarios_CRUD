7512a66bf7dcac2ce4a9f96990ee89ed05afb58b
Brayan Reyes <bryanreyes47k@gmail.com>
2026-04-28 13:30:08 -0500

    se crea modelo de contrasena para el api yoplay

---
+++ models/contrasena.go
@@ -0,0 +1,15 @@
+package models
+
+import ( 
+	"Time"
+)
+
+type Contrasena struct{
+	ID int `json:"id_contrasena"` // campo id 
+	Contrasena string `json:"contrasena"`
+	Hash_contrasena string `json:"hash_contrasena"`
+	ID_usuario int `json:"id_usuario"`
+	Activo bool `json:"activo"`
+	Fecha_creacion time.Time `json:"fecha_creacion"`
+	Fecha_modificacion time.Time `json:"fecha_modificacion"`
+}
