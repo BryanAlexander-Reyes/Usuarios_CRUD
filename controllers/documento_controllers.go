897594d27f2f6eeeee6d44d52b01a446fdf4427d
Brayan Reyes <bryanreyes47k@gmail.com>
2026-04-28 15:48:18 -0500

    se crea controllador DeleteDocuments de usuario-documento para el api yoplay

--- controllers/documento_controllers.go
+++ controllers/documento_controllers.go
@@ -92,4 +92,16 @@ func UpdateDocumnts(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, map[string]string{"Message": "Dato Actualizado"})
 }
+// DeleteUser eliminar usuario
+func DeleteDocuments(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	
+	_, err := config.DB.Exec("DELETE FROM documento WHERE id = $1", id)
 
+	if err != nil {
+		respondJSON(w, 500,map[string]string{"Error":"Error"})
+		return
+	}
+	respondJSON(w, 200, map[string]string{"Message": "Dato eliminado"})
+}
